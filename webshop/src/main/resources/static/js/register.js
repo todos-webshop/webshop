434f69d5c823f22b574e0b6b822775ace22dc866
Pál Gábor <gaborpal1992@gmail.com>
2019-03-21 10:48:08 +0100

    password strengthcheck

--- webshop/src/main/resources/static/js/register.js
+++ webshop/src/main/resources/static/js/register.js
@@ -41,3 +41,27 @@ function createUser() {
   }
   return false;
 }
+var strength = {
+  0: "Worst",
+  1: "Bad",
+  2: "Weak",
+  3: "Good",
+  4: "Strong"
+}
+var password = document.getElementById("pass-input1");
+var meter = document.getElementById('password-strength-meter');
+var text = document.getElementById('password-strength-text');
+
+password.addEventListener('input', function() {
+  var val = password.value;
+  var result = zxcvbn(val);
+
+  meter.value = result.score;
+
+
+  if (val !== "") {
+    text.innerHTML = "Strength: " + strength[result.score];
+  } else {
+    text.innerHTML = "";
+  }
+});
