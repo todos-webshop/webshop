22d98af1aed59f848b151d46de6cfea64f08adb6
gkevin94 <gkevin94@gmail.com>
2019-03-21 17:55:56 +0100

    menu.js update

--- webshop/src/main/resources/static/js/menu.js
+++ webshop/src/main/resources/static/js/menu.js
@@ -1,21 +1,20 @@
-var user;
-
+var user = getUser();
+// getUser();
+setMenu();
 window.onload = function(){
-//    getUser();
-    setMenu();
+    
 }
 
-// function getUser(){
-//     var url = "/username";
-//     fetch(url)
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(function(jsonData){
-//         user = jsonData;
-//         console.log(user);
-//     })
-// }
+function getUser(){
+    var url = "/userdata";
+    return fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(jsonData){
+        return jsonData;
+    })
+}
 
 function setMenu(){
     var body = document.getElementsByTagName("body")[0];
@@ -24,17 +23,50 @@ function setMenu(){
     div.setAttribute("class", "menu_div");
 
     div.appendChild(createLogoImg());
-    div.appendChild(createOrdersButton());
-    div.appendChild(createHistoryButton());
-    div.appendChild(createStatButton());
+    // div.appendChild(createOrdersButton());
+    // div.appendChild(createHistoryButton());
+    // div.appendChild(createStatButton());
     div.appendChild(createLoginButton());
     div.appendChild(createSignUpButton());
-    div.appendChild(createCartButton());
     div.appendChild(createLogoutButton());
+    console.log(user);
+    // switch(user.role){
+    //     case "NOT_AUTHENTICATED": break;
+    //     case "USER": {
+    //         createDropdownDivForUser(div);
+    //         break;
+    //     }
+    //     case "ADMIN": {
+    //         createDropdownDivForAdmin(div);
+    //         break;
+    //     }
+    // }
+    
+    createDropdownDivForUser(div);
+    createDropdownDivForAdmin(div);    
+    div.appendChild(createCartButton());
+
 
     body.insertBefore(div, body.firstChild);
     setFavicon();
     insertMenuCss();
+}
+
+function classChanger() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
 
 function setFavicon(){
@@ -60,6 +92,34 @@ function insertMenuCss(){
     head.appendChild(cssLink);
 }
 
+function createDropdownDivForUser(div){
+    div.innerHTML += `
+    <div class="dropdown">
+        <button onclick="classChanger()" class="dropbtn">User</button>
+        <div id="myDropdown" class="dropdown-content">
+            <a href="/orders.html">Orders</a>
+            <a href="/user.html">Profile</a>
+            <a href="#contact">Contact</a>
+        </div>
+    </div>
+    `;
+}
+
+function createDropdownDivForAdmin(div){
+    div.innerHTML += `
+    <div class="dropdown">
+        <button onclick="classChanger()" class="dropbtn">Admin</button>
+        <div id="myDropdown" class="dropdown-content">
+            <a href="/history.html">Order history</a>
+            <a href="/statistics.html">Statistics</a>
+            <a href="/users.html">Users</a>
+        </div>
+    </div>
+    `;
+}
+
+
+
 function createLogoImg(){
     var logoImg = document.createElement("img");
     logoImg.setAttribute("src", "img/logo.png");
@@ -71,32 +131,32 @@ function createLogoImg(){
     return logoImg;
 }
 
-function createOrdersButton(){
-    var ordersBtn = document.createElement("button");
-    ordersBtn.innerText = "Orders";
-    ordersBtn.onclick = function(){
-        window.location.href = "orders.html";
-    }
-    return ordersBtn;
-}
+// function createOrdersButton(){
+//     var ordersBtn = document.createElement("button");
+//     ordersBtn.innerText = "Orders";
+//     ordersBtn.onclick = function(){
+//         window.location.href = "orders.html";
+//     }
+//     return ordersBtn;
+// }
 
-function createHistoryButton(){
-    var historyBtn = document.createElement("button");
-    historyBtn.innerText = "History";
-    historyBtn.onclick = function(){
-        window.location.href = "history.html";
-    }
-    return historyBtn;
-}
+// function createHistoryButton(){
+//     var historyBtn = document.createElement("button");
+//     historyBtn.innerText = "History";
+//     historyBtn.onclick = function(){
+//         window.location.href = "history.html";
+//     }
+//     return historyBtn;
+// }
 
-function createStatButton(){
-    var statisticsBtn = document.createElement("button");
-    statisticsBtn.innerText = "Statistics";
-    statisticsBtn.onclick = function(){
-        window.location.href = "statistics.html";
-    }
-    return statisticsBtn;
-}
+// function createStatButton(){
+//     var statisticsBtn = document.createElement("button");
+//     statisticsBtn.innerText = "Statistics";
+//     statisticsBtn.onclick = function(){
+//         window.location.href = "statistics.html";
+//     }
+//     return statisticsBtn;
+// }
 
 function createLoginButton(){
     var loginBtn = document.createElement("button");
@@ -123,7 +183,7 @@ function createSignUpButton(){
         window.location.href = "/register.html";
     }
     return signUpBtn;
-} 
+}
 
 function createCartButton(){
     var cartBtn = document.createElement("button");
