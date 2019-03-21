eaf4daa3277a3ccfe6826a74aaa66f0047050534
Fanni Tormasi <tormasifanni@gmail.com>
2019-03-21 12:53:56 +0100

    committing product update

--- webshop/src/main/java/webshop/product/ProductController.java
+++ webshop/src/main/java/webshop/product/ProductController.java
@@ -17,41 +17,39 @@ public class ProductController {
 
     private ProductValidator productValidator = new ProductValidator();
 
-    @GetMapping("api/products")
-    public List<Product> listAllProducts(){
+    @GetMapping("/api/products")
+    public List<Product> listAllProducts() {
         return productService.listAllProducts();
     }
 
 
-    @PostMapping("api/product/{address}")
-    public void ProductShow(@PathVariable String addresss, @RequestBody Product product){
-    }
-
-    @GetMapping("product/{address}")
-    public Product findProductByAddress(@PathVariable String address){
+    @GetMapping("/api/products/{address}")
+    public Product findProductByAddress(@PathVariable String address) {
         return productService.findProductByAddress(address);
     }
 
-    @PostMapping("api/products")
-    public CustomResponseStatus addNewProduct(@RequestBody Product product){
+    @PostMapping("/api/products")
+    public CustomResponseStatus addNewProduct(@RequestBody Product product) {
         try {
             CustomResponseStatus responseStatus = productValidator.validateProduct(product);
-            if (responseStatus.getResponse().equals(Response.SUCCESS)){
+            if (responseStatus.getResponse().equals(Response.SUCCESS)) {
                 long id = productService.addNewProductAndGetId(product);
                 return new CustomResponseStatus(Response.SUCCESS, String.format("Successfully created with %d id", id));
             } else {
                 return responseStatus;
             }
-        } catch (IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
             return new CustomResponseStatus(Response.FAILED, iae.getMessage());
         }
     }
-    @PostMapping("api/product/{id}")
-    public ProductResponseStatus updateProduct(@PathVariable Long id, @RequestBody Product product){
-        if (product.getName()==null|| product.getName().trim().equals("")){
-            return new ProductResponseStatus(false,"Product name empty");
-        }
-        productService.updateProduct(id,product.getCode(),product.getName(),product.getAddress(),product.getManufacturer(),product.getPrice());
-    return new ProductResponseStatus(true,"Minden ok");
+
+    @PostMapping("/api/products/{productId}")
+    public CustomResponseStatus updateProduct(@PathVariable Long productId, @RequestBody Product product) {
+            CustomResponseStatus responseStatus = productValidator.validateProduct(product);
+            if (responseStatus.getResponse().equals(Response.FAILED)) {
+                return responseStatus;
+            } else {
+                return productService.updateProduct(product, productId);
+            }
     }
 }
