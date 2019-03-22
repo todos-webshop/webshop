00c0a656002c1f58806642ac4f7ed99129f069f0
Fanni Tormasi <tormasifanni@gmail.com>
2019-03-22 16:01:33 +0100

    new features

--- webshop/src/main/resources/static/js/products.js
+++ webshop/src/main/resources/static/js/products.js
@@ -21,66 +21,46 @@ function fetchProducts() {
        for (var i = 0; i < jsonData.length; i++) {
        var divRow = document.createElement("div");
 
-<<<<<<< HEAD
        var codeDiv = document.createElement("div");
        codeDiv.innerHTML = jsonData[i].code;
-       codeDiv.setAttribute('class', 'div_class');
+       codeDiv.setAttribute('class', 'div_class_user');
        divRow.appendChild(codeDiv);
 
        var nameDiv = document.createElement("div");
        nameDiv.innerHTML = jsonData[i].name;
-       nameDiv.setAttribute('class', 'div_class');
+       nameDiv.setAttribute('class', 'div_class_user');
        divRow.appendChild(nameDiv);
 
        var addressDiv = document.createElement("div");
        addressDiv.innerHTML = jsonData[i].address;
-       addressDiv.setAttribute('class', 'div_class');
+       addressDiv.setAttribute('class', 'div_class_user');
        divRow.appendChild(addressDiv);
 
        var manufacturerDiv = document.createElement("div");
        manufacturerDiv.innerHTML = jsonData[i].manufacturer;
-       manufacturerDiv.setAttribute('class', 'div_class');
+       manufacturerDiv.setAttribute('class', 'div_class_user');
        divRow.appendChild(manufacturerDiv);
 
        var priceDiv = document.createElement("div");
        priceDiv.innerHTML = jsonData[i].price+ " Ft";
-       priceDiv.setAttribute('class', 'div_class');
+       priceDiv.setAttribute('class', 'div_class_user');
        divRow.appendChild(priceDiv);
 
        var statusDiv = document.createElement('div');
        statusDiv.innerHTML = jsonData[i].productStatus;
-       statusDiv.setAttribute('class', 'div_class');
+       statusDiv.setAttribute('class', 'div_class_user');
        divRow.appendChild(statusDiv);
 
-       divRow.appendChild(imgDiv);
+       divMain.appendChild(divRow);
+
+
+       nameDiv.setAttribute("onclick", `window.location.href="product.html?address=${jsonData[i].address}"`);
+       priceDiv.setAttribute("onclick", `window.location.href="product.html?address=${jsonData[i].address}"`);
+       codeDiv.setAttribute("onclick", `window.location.href="product.html?address=${jsonData[i].address}"`);
+       addressDiv.setAttribute("onclick", `window.location.href="product.html?address=${jsonData[i].address}"`);
+       statusDiv.setAttribute("onclick", `window.location.href="product.html?address=${jsonData[i].address}"`);
 
        divMain.appendChild(divRow);
-=======
-
-            var nameDiv = document.createElement("div");
-
-                   nameDiv.innerHTML = jsonData[i].name;
-                    nameDiv.setAttribute('class', 'div_class');
-
-                     nameDiv.setAttribute("onclick", `window.location.href="product.html?address=${jsonData[i].address}"`);
-                   divRow.appendChild(nameDiv);
-
-
-           var priceDiv = document.createElement("div");
-                           priceDiv.innerHTML = jsonData[i].price+ " Ft";
-                            priceDiv.setAttribute('class', 'div_class');
-
-                            priceDiv.setAttribute("onclick", `window.location.href="product.html?address=${jsonData[i].address}"`);
-                           divRow.appendChild(priceDiv);
-
-            var imgDiv = document.createElement("div");
-                                    imgDiv.innerHTML = "<img alt="+jsonData[i].address+" src=img\\products\\"+jsonData[i].address+".png>";
-                                   imgDiv.classList.add('div_class');
-                                   imgDiv.setAttribute("onclick", `window.location.href="product.html?address=${jsonData[i].address}"`);
-                                    divRow.appendChild(imgDiv);
-
-           divMain.appendChild(divRow);
->>>>>>> 1aa09cd231d994e2624407376dccbfd6425d45f2
 
    }
 
