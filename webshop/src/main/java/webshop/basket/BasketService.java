5867d2429761ba6b5e8680a2241aa04131f3320f
holdudvar <email@mail.com>
2019-03-22 14:24:33 +0100

    add to basket by product CODE works from PostMan (POST to /basket) - only 1 piece of a product can be added at the moment

--- webshop/src/main/java/webshop/basket/BasketService.java
+++ webshop/src/main/java/webshop/basket/BasketService.java
@@ -3,6 +3,8 @@ package webshop.basket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import webshop.product.Product;
+import webshop.product.ProductDao;
+import webshop.product.ProductData;
 import webshop.user.User;
 import webshop.user.UserDao;
 import webshop.user.UserData;
@@ -12,13 +14,14 @@ import java.util.List;
 @Service
 public class BasketService {
 
-    //    @Autowired
     private BasketDao basketDao;
     private UserDao userDao;
+    private ProductDao productDao;
 
-    public BasketService(BasketDao basketDao, UserDao userDao) {
+    public BasketService(BasketDao basketDao, UserDao userDao, ProductDao productDao) {
         this.basketDao = basketDao;
         this.userDao = userDao;
+        this.productDao = productDao;
     }
 
     // user id is used from here (instead of username) in case username changes in time
@@ -27,13 +30,7 @@ public class BasketService {
         User user = userDao.getUserByUsername(loggedInUsername);
 
         long userId = user.getId();
-        long basketId = 0;
-
-        if (!basketDao.getAllBasketOwnerIds().contains(userId)) {
-            basketId = basketDao.createBasketForUserIdAndReturnBasketId(userId);
-        } else {
-            basketId = basketDao.getBasketIdByUserId(userId);
-        }
+        long basketId = getOrCreateAndReturnBasketIdByUserId(userId);
 
         if (basketId == 0) {
             throw new IllegalStateException("Basket does not exist.");
@@ -50,8 +47,25 @@ public class BasketService {
         int sumPieces = basketDao.sumProductPiecesInBasketByBasketId(basketId);
         int sumPrice = basketDao.sumProductPriceInBasketByBasketId(basketId);
 
-        return new BasketData(sumPieces, sumPrice,  basket);
+        return new BasketData(sumPieces, sumPrice, basket);
     }
 
+
+    public int addProductToLoggedInBasketByProductData(String loggedInUsername, ProductData productData) {
+        long userId = userDao.getUserByUsername(loggedInUsername).getId();
+        long basketId = getOrCreateAndReturnBasketIdByUserId(userId);
+        int quantity = productData.getProductPieces();
+        long productId = productDao.getProductIdByProductCode(productData.getProductCode());
+        return basketDao.addProductToBasket(basketId, productId, quantity);
+    }
+
+
+    private long getOrCreateAndReturnBasketIdByUserId(long userId) {
+        if (!basketDao.getAllBasketOwnerIds().contains(userId)) {
+            return basketDao.createBasketForUserIdAndReturnBasketId(userId);
+        } else {
+            return basketDao.getBasketIdByUserId(userId);
+        }
+    }
 
 }
