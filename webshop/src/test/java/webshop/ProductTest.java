dd3d90e544f88544174184aee21017c93b64f9ab
lammoni <lammoni85@gmail.com>
2019-03-22 15:47:31 +0100

    test...

---
+++ webshop/src/test/java/webshop/ProductTest.java
@@ -0,0 +1,37 @@
+package webshop;
+
+import org.junit.Test;
+import webshop.product.Product;
+import webshop.product.ProductStatus;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class ProductTest {
+
+
+
+    @Test
+    public void testCreateProduct() {
+        Product product = new Product(1266, "H59L", "something good", "manufact", 1255,
+        ProductStatus.ACTIVE );
+
+        assertThat(product.getAddress(), equalTo("something_good"));
+        assertThat(product.getProductStatus(), equalTo(ProductStatus.ACTIVE));
+    }
+
+    @Test
+    public void testNormalize() {
+        Product product = new Product(1266, "H59L", "árvíztűrő tükörfúrógép", "manufact", 1255,
+                ProductStatus.ACTIVE );
+        Product product2 = new Product(1266, "H59L", "SoMeThInG   Goood", "manufact", 1255,
+                ProductStatus.ACTIVE );
+        Product product3 = new Product(1266, "H59L", "elkkelkáposztástalanítottátok", "manufact", 1255,
+                ProductStatus.ACTIVE );
+
+        assertThat(product.getAddress(), equalTo("arvizturo_tukorfurogep"));
+        assertThat(product3.getAddress(), equalTo("elkkelkaposztastalanitottatok"));
+        assertThat(product2.getAddress(), equalTo("something_good"));
+    }
+
+}
