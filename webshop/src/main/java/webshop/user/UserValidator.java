5dc03d039f54ce68f8fa3dbad3f3ab2aef9e9724
gkevin94 <gkevin94@gmail.com>
2019-04-08 09:13:34 +0200

    smells better

--- webshop/src/main/java/webshop/user/UserValidator.java
+++ webshop/src/main/java/webshop/user/UserValidator.java
@@ -7,12 +7,6 @@ import webshop.validator.Validator;
 @Component
 public class UserValidator implements Validator {
 
-    private UserService userService;
-
-    public UserValidator(UserService userService) {
-        this.userService = userService;
-    }
-
     public boolean userCanBeUpdated(User user) {
         return !isEmpty(user.getFirstName()) && !isEmpty(user.getPassword()) && !isEmpty(user.getLastName()) && isUsernameValid(user) && passwordIsValid(user.getPassword());
     }
